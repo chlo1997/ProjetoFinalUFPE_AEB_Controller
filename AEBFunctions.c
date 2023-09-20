f0d44be275398289ca98d04e7636cda643b48b53
Lucas Gabriel <lucas.morais@cear.ufpb.br>
2023-09-19 22:01:22 -0300

    StoppingTimeCalculation update

--- AEBFunctions.c
+++ AEBFunctions.c
@@ -2,20 +2,20 @@
 @brief Estimates current vehicle stopping time based on current vehicle speed, road conditions, adds driver reaction time
 
 @param[in] VehSpeed float. Actual Vehicle Speed
-@param[in] roadConditionCoeff float. Percentage of friction coefficient of the road based on vehicle measured climate conditions (wet or dry)
+@param[in] roadCondCoeff float. Percentage of friction coefficient of the road based on vehicle measured climate conditions (wet or dry)
 @param[in] driverTimeReact float. Configurable additional Driver's Time Reaction to press Brake pedal 
 @param[in] PBDecel float. Configurable Partial Braking Deceleration used by AEB as deceleration reference
 @param[in] FBDecel float. Configurable Full Braking Deceleration used by AEB as deceleration reference
  **/
-float *StoppingTimeCalculation(float VehSpeed,float roadConditionCoeff,float driverTimeReact,float PBDecel, float FBDecel)
+float *StoppingTimeCalculation(float VehSpeed,float roadCondCoeff, float driverTimeReact, float PBDecel, float FBDecel)
 {   
-    // Inicialization of the variables
+    // Initialization of the variables
     static float StoppingTime[2];
     float PBStoppingTime = 0;
     float FBStoppingTime = 0;
 
     // Calculation for Stopping Time
-    PBStoppingTime = (VehSpeed / (PBDecel * roadCondCoeff))  + driverTimeReact; // Formula for parcial breaking time
+    PBStoppingTime = (VehSpeed / (PBDecel * roadCondCoeff))  + driverTimeReact; // Formula for partial breaking time
     FBStoppingTime = (VehSpeed / (FBDecel * roadCondCoeff))  + driverTimeReact; // Formula for full breaking time
 
     StoppingTime[0] = PBStoppingTime;
