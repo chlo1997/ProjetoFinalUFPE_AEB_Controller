7e6682f1cb19a7207f5d7a46a8127c90bea3fc19
1lucas1gabriel <51426299+1lucas1gabriel@users.noreply.github.com>
2023-10-05 00:55:21 +0000

    Deploying to gh-pages from @ chlo1997/ProjetoFinalUFPE_AEB_Controller@322dc62571506519a4ebe50eaaae3be9e06a4593 🚀

---
+++ doc/html/search/all_d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['soundalertcontrol_35',['soundAlertControl',['../AEBFunctions_8h.html#a6df1643d5df9aa77813d263f0f8963d3',1,'soundAlertControl(enum enum_AEBStatus AEBStatus):&#160;AEBFunctions.c'],['../AEBFunctions_8c.html#a6df1643d5df9aa77813d263f0f8963d3',1,'soundAlertControl(enum enum_AEBStatus AEBStatus):&#160;AEBFunctions.c']]],
+  ['stoppingtimecalculation_36',['StoppingTimeCalculation',['../AEBFunctions_8h.html#ae4f796faefc37b4794493cf97ba1fc7c',1,'StoppingTimeCalculation(float VehSpeed, float roadCondCoeff, float driverTimeReact, float PBDecel, float FBDecel):&#160;AEBFunctions.c'],['../AEBFunctions_8c.html#ae4f796faefc37b4794493cf97ba1fc7c',1,'StoppingTimeCalculation(float VehSpeed, float roadCondCoeff, float driverTimeReact, float PBDecel, float FBDecel):&#160;AEBFunctions.c']]]
+];
